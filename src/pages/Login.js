ea65011e08415cb7c379db143afbb545e6fe59ef
Abdullah Basit <abdullah.basit@hotmail.com>
2021-03-09 11:11:12 +0500

    Fixed login page layout. (2)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -98,49 +98,52 @@ export default function Login(props) {
                     />}
                 </Col>
             </Row>
-            <Form
-                {...layout}
-                name="basic"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-            >
-                <Form.Item
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your username!',
-                        },
-                    ]}
-                >
-                    <Input prefix={<UserOutlined />} placeholder="Username" />
-                </Form.Item>
+            <Row>
+                <Col span={8} offset={8}>
+                    <Form
+                        name="basic"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your username!',
+                                },
+                            ]}
+                        >
+                            <Input prefix={<UserOutlined />} placeholder="Username" />
+                        </Form.Item>
 
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password prefix={<LockOutlined />} placeholder="Password" />
-                </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                        >
+                            <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+                        </Form.Item>
 
-                <Form.Item name="remember" valuePropName="checked">
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
+                        <Form.Item name="remember" valuePropName="checked">
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
 
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" block>
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" block>
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
         </Spin>
     )
 }
